bc7a376ff0325b3fa2b10583cc487ce78a49e91a
DEETS\StephenF <sfnhmp@gmail.com>
2016-08-28 18:00:05 -0600

    Got AI possessing tanks placed in the scene

---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,21 @@
+// copyright@Saltwater Games 2016
+
+#pragma once
+
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"     // Must be the last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank* GetControlledTank() const;
+	
+	virtual void BeginPlay() override;
+};
